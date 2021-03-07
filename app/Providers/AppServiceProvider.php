9fde80241a4c6582a16665dd51073de2b0735413
Abdul-Wahab-Khan <ab.wahab.kh@gmail.com>
2021-03-07 11:41:15 +0430

    Env changed

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,9 +25,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        if ($this->app->environment('local')) {
-            $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
-            $this->app->register(TelescopeServiceProvider::class);
-        }
+
     }
 }
