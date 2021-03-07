b4393d815c93087ab7864db24b31abd943906b4a
Abdul-Wahab-Khan <ab.wahab.kh@gmail.com>
2021-03-07 14:40:36 +0430

    Again changes in database.php files

--- config/database.php
+++ config/database.php
@@ -3,10 +3,10 @@
 use Illuminate\Support\Str;
 
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$host = 'localhost';
-$username = 'root';
-$password = 'yoursql';
-$database = 'laravel';
+$host = localhost;
+$username = $url["user"] ?? null;
+$password = $url["pass"] ?? null;
+$database = substr($url["path"], 1);
 
 return [
 
@@ -54,9 +54,9 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => $database,
-            'username' => $username,
-            'password' => $password,
+            'database' => env('DB_DATABASE', 'laravel'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', 'yoursql'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
